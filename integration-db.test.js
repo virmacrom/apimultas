67e545948a10f315935c8f6e750f0e2c29ee3fa9
igndeldel <58361976+igndeldel@users.noreply.github.com>
2020-01-02 23:40:41 +0100

    en una carpeta test con server.test

---
+++ integration-db.test.js
@@ -0,0 +1,32 @@
+const Multa = require ('../multas');
+const mongoose = require ('mongoose');
+const dbConnect = require ('../db');
+
+describe('Multas DB connection', () =>{
+    beforeAll(() =>{
+        return dbConnect();
+    });
+
+    beforeEach((done) =>{
+        Multa.deleteMany({}, (err) =>{
+            done();
+        });
+    });
+
+    it ('writes a multas in the DB', (done)=>{
+        const multa = new Multa({dni:'12345678A', puntos:'3', name:'alcohol'});
+        multa.save((err, multa) =>{
+            expect(err).toBeNull();
+            Multa.find({}, (err, multas)=>{
+                expect(multas).toBeArrayOfSize(1);
+                done();
+            });
+        });
+    }, 300000);
+
+    afterAll((done) =>{
+        mongoose.connection.db.dropDatabase(()=>{
+            mongoose.connection.close(done);
+        });
+    });
+});
