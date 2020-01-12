51f4aa4c6ed00ae946cdc73d2e6c39de21179c8c
igndeldel <58361976+igndeldel@users.noreply.github.com>
2020-01-12 19:39:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ var bodyParser = require('body-parser');
 
 const Multa = require ('./multas');
 const passport = require ('passport');
+const swaggerDoc = require ('./swagger-doc');
 
 require('./passport.js');
 
 var BASE_API_PATH = "/api/v1";
 
 var app = express();
+app.use(bodyParser.urlencoded({extended: false})) //añadida
 app.use(bodyParser.json());
 app.use(passport.initialize());
+
+swaggerDoc(app);
 
 app.get("/",(req,res)=>{
     res.send("<html><body><h1>My server multas</h1></body></html>")
@@ -56,39 +60,38 @@ app.get(BASE_API_PATH + "/multas/:dni",
     });
 });
 
+
+app.get(BASE_API_PATH + "/multas/nummultas/:dni", 
+    passport.authenticate('localapikey', {session:false}),
+    (req, res) =>{
+        var dni = req.params.dni;
+          console.log(Date() + " - GET /multas/nummultas/:dni");
+          
+          Multa.find({"dni":dni},(err, multas) =>{
+              let contador=0;
+            if(err){
+                console.log(Date () + " - " + err);
+                res.sendStatus(500);
+            }else{
+                res.send(multas.map((multa) => {
+                    contador++;
+                   console.log("números de multas con el dni " + dni +" : " +contador);
+                   // return multa.toJSON;
+
+                }));
+                console.log(contador);
+                res =contador;
+                return contador;
+                
+            }
+        });
+});
+
 app.post(BASE_API_PATH + "/multas", (req, res) =>{
     console.log(Date() + " - POST /multas");
     var multa = req.body;
 
-    /*VALIDACION DEL DNI
-    
-    var numero
-    var letr
-    var letra
-    var expresion_regular_dni = multa.dni;
- 
-    expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
- 
-     if(expresion_regular_dni.test (dni) == true){
-            numero = dni.substr(0,dni.length-1);
-            letr = dni.substr(dni.length-1,1);
-            numero = numero % 23;
-            letra='TRWAGMYFPDXBNJZSQVHLCKET';
-            letra=letra.substring(numero,numero+1);
-             if (letra!=letr.toUpperCase()) {
-                alert('Dni erroneo, la letra del NIF no se corresponde');
-            }else{
-                 alert('Dni correcto');
-           }
-        } else{
-              alert('Dni erroneo, formato no válido');
-        }
-
-    
-    /*FIN DE LA VALIDACION DEL DNI*/
-
-
-    //ya no es db.insert, ahora es multa.create
+   //ya no es db.insert, ahora es multa.create
     Multa.create(multa, (err) => {
         if (err){
             console.log (Date () + " - " + err);
@@ -100,49 +103,37 @@ app.post(BASE_API_PATH + "/multas", (req, res) =>{
   //  multas.push(multa);
    // res.sendStatus(201);
 });
-/*
+
 app.put(BASE_API_PATH + "/multas/editar", (req, res) => {
     console.log(Date()+" - PUT /multas/editar");
     res.sendStatus(405);
-});*/
+});
 
 app.put(BASE_API_PATH + "/multas/editar/:dni", (req, res) => {
-    console.log(Date()+" - PUT /multas/editar");
-    
-    let nuevoDni=req.params.dni;
-    let update = req.body
+    var dni = req.params.dni;
+    var updatedMultas = req.body;
+    console.log(Date()+" - PUT /multas/editar/"+dni);
 
-    console.log(req.body); 
+    if(dni != updatedMultas.dni){
+        console.log("El DNI a actualizar no existe en la BD");
+        res.sendStatus(409);
+        return;
+    }
 
-    Multa.findByIdAndUpdate(req.body.dni, update, (err, multaActualizada)=>{
-        if (err) {
-            return res.status(500).send({message: "Error al actualizar"})
-            
-        }else {
-            return res.status(200).send({multa: "Multa actualizada"})
+    Multa.update({"dni": dni},updatedMultas, (err,updateResult)=>{
+        if(err){
+            console.error("Error accediendo a la BD");
+            res.sendStatus(500);
+        }else{
+            if(updateResult.n>1){
+                console.warn("Inconsistencia en la BD: nombre duplicado");
+            }else if(updateResult.n == 0) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
         }
     });
-    
-   
-    // if(dni != updatedMultas.dni){
-    //     res.sendStatus(409);
-    //     return;
-    // }
-
-    // db.update({"dni": dni},updatedMultas, (err,updateResult)=>{
-    //     if(err){
-    //         console.error("Error accesing DB");
-    //         res.sendStatus(500);
-    //     }else{
-    //         if(updateResult.n>1){
-    //             console.warn("Incosistent DB: duplicated name");
-    //         }else if(updateResult.n == 0) {
-    //             res.sendStatus(404);
-    //         } else {
-    //             res.sendStatus(200);
-    //         }
-    //     }
-    // });
 });
 
 app.delete(BASE_API_PATH + "/multas", (req, res) => {
